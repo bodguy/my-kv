a9ae25ed54053e05329a8142eb239f0368c2734d
bodguy <alsrudgns99@gmail.com>
2023-05-10 00:21:08 +0900

    simple client

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,5 +1,43 @@
-#include <iostream>
+#include <cstdint>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+#include <cerrno>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <spdlog/spdlog.h>
 
-int main(int argc, char** argv) {
-    std::cout << "Hello world" << std::endl;
+static void die(const char *msg) {
+    int err = errno;
+    spdlog::error("[{}] {}", err, msg);
+    abort();
+}
+
+int main() {
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd < 0) {
+        die("socket()");
+    }
+
+    struct sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_port = ntohs(9200);
+    addr.sin_addr.s_addr = ntohl(INADDR_LOOPBACK);  // 127.0.0.1
+    int rv = connect(fd, (const struct sockaddr *)&addr, sizeof(addr));
+    if (rv) {
+        die("connect");
+    }
+
+    char msg[] = "hello";
+    write(fd, msg, strlen(msg));
+
+    char rbuf[64] = {};
+    ssize_t n = read(fd, rbuf, sizeof(rbuf) - 1);
+    if (n < 0) {
+        die("read");
+    }
+    printf("server says: %s\n", rbuf);
+    close(fd);
+    return 0;
 }
