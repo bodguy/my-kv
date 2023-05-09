db9f34035e2155a5f69d0d047d1a27779511639b
bodguy <alsrudgns99@gmail.com>
2023-05-10 00:18:55 +0900

    gtest, server/client module add

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -2,5 +2,4 @@
 
 int main(int argc, char** argv) {
     std::cout << "Hello world" << std::endl;
-    return 0;
 }
---
+++ src/server/main.cpp
@@ -0,0 +1,74 @@
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+#include <cerrno>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <spdlog/spdlog.h>
+
+static void msg(const char *msg) {
+    spdlog::warn("{}", msg);
+}
+
+static void die(const char *msg) {
+    int err = errno;
+    spdlog::error("[{}] {}", err, msg);
+    abort();
+}
+
+static void do_something(int connfd) {
+    char rbuf[64] = {};
+    ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1);
+    if (n < 0) {
+        msg("read() error");
+        return;
+    }
+    printf("client says: %s\n", rbuf);
+
+    char wbuf[] = "world";
+    write(connfd, wbuf, strlen(wbuf));
+}
+
+int main() {
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd < 0) {
+        die("socket()");
+    }
+
+    // this is needed for most server applications
+    int val = 1;
+    setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
+
+    // bind
+    struct sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_port = ntohs(9200);
+    addr.sin_addr.s_addr = ntohl(0);    // wildcard address 0.0.0.0
+    int rv = bind(fd, (const sockaddr *)&addr, sizeof(addr));
+    if (rv) {
+        die("bind()");
+    }
+
+    // listen
+    rv = listen(fd, SOMAXCONN);
+    if (rv) {
+        die("listen()");
+    }
+
+    while (true) {
+        // accept
+        struct sockaddr_in client_addr = {};
+        socklen_t socklen = sizeof(client_addr);
+        int connfd = accept(fd, (struct sockaddr *)&client_addr, &socklen);
+        if (connfd < 0) {
+            spdlog::warn("{} accept", connfd);
+            continue;   // error
+        }
+
+        do_something(connfd);
+        close(connfd);
+    }
+
+    return 0;
+}
